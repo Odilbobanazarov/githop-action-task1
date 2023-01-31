58730e02487d72fce97780e4d0357ec21e908a26
Odilbobanazarov <116312437+Odilbobanazarov@users.noreply.github.com>
2023-01-31 12:11:09 -0600

    Create private_subnet.tf

---
+++ private_subnet.tf
@@ -0,0 +1,36 @@
+resource "aws_subnet" "private1" {
+vpc_id           = aws_vpc.main.id
+cidr_block       = "10.0.4.0/24"
+availability_zone = "eu-west-1a"
+
+
+
+
+
+
+
+  # Please do not change below code
+  tags = local.task_tags
+}
+
+
+resource "aws_subnet" "private2" {
+vpc_id           = aws_vpc.main.id
+cidr_block       = "10.0.5.0/24"
+availability_zone = "eu-west-1b"
+
+
+
+
+
+  # Please do not change below code
+  tags = local.task_tags
+}
+
+
+
+resource "aws_subnet" "private3" {
+vpc_id           = aws_vpc.main.id
+cidr_block       = "10.0.6.0/24"
+availability_zone = "eu-west-1c"
+
